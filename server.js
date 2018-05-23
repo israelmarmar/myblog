c24b8e9db47c6421120481c50cf84db54437b402
israelmarmar <email.israel.br@gmail.com>
2018-05-23 13:24:02 -0400

    new files

--- server.js
+++ server.js
@@ -8,25 +8,54 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-var postSchema = new mongoose.Schema({ body: String });
+var postSchema = new mongoose.Schema({title: String, subtitle: String, idtitle: String, body: String });
 var Post = mongoose.model('Post', postSchema);
+var marked = require ('marked');
+
 // Routes
 
 
+app.get("/:post", (req, res) => {
+
+	var md = function (txt) {
+
+      var html = marked (txt);
+
+      return html;
+   };
+
+
+	Post.find({idtitle:req.params.post}, (err, posts) => {
+	res.render('post', { post: posts[0], md: md});
+	});
+
+});
+
+
 app.get("/", (req, res) => {
+
 	Post.find({}, (err, posts) => {
 		console.log(posts);
-		res.render('index', { posts: posts})
+		res.render('index', { posts: posts});
 	});
+
 });
-app.post('/addpost', (req, res) => {
+
+
+app.post('/add/post', (req, res) => {
 	console.log(req.body);
+	req.body.idtitle=req.body.title.toLowerCase().replace(/\s/g, '-');
+	console.log(req.body.idtitle);
 	var postData = new Post(req.body);
 	postData.save().then( result => {
 		res.redirect('/');
 	}).catch(err => {
 		res.status(400).send("Unable to save data");
 	});
+});
+
+app.get('/add/post', (req, res) => {
+	 res.sendFile("/addpost.html",{root: __dirname+"/views"});
 });
 
 
