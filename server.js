c3d770b18cb382cd8cbcc9b544cfe5ca17287bf5
israelmarmar <email.israel.br@gmail.com>
2018-05-24 13:34:07 -0400

    update

--- server.js
+++ server.js
@@ -8,11 +8,36 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-var postSchema = new mongoose.Schema({title: String, subtitle: String, idtitle: String, body: String });
+var postSchema = new mongoose.Schema({title: String, subtitle: String, idtitle: String, date: String, body: String });
 var Post = mongoose.model('Post', postSchema);
 var marked = require ('marked');
+var dateFormat = require('dateformat');
 
 // Routes
+
+/*
+app.get('/*.*', function(req, res) {
+
+});
+*/
+
+app.post('/addpost', (req, res) => {
+	console.log(req.body);
+	var day=dateFormat(new Date(), "yyyy/mm/dd h:MM:ss").toString();
+	req.body.idtitle=req.body.title.toLowerCase().replace(/\s/g, '-');
+	req.body.date=day;
+
+	var postData = new Post(req.body);
+	postData.save().then( result => {
+		res.redirect('/');
+	}).catch(err => {
+		res.status(400).send("Unable to save data");
+	});
+});
+
+app.get('/addpost', (req, res) => {
+	 res.sendFile("/addpost.html",{root: __dirname+"/views"});
+});
 
 
 app.get("/:post", (req, res) => {
@@ -29,6 +54,7 @@ app.get("/:post", (req, res) => {
 	res.render('post', { post: posts[0], md: md});
 	});
 
+
 });
 
 
@@ -41,24 +67,8 @@ app.get("/", (req, res) => {
 
 });
 
-
-app.post('/add/post', (req, res) => {
-	console.log(req.body);
-	req.body.idtitle=req.body.title.toLowerCase().replace(/\s/g, '-');
-	console.log(req.body.idtitle);
-	var postData = new Post(req.body);
-	postData.save().then( result => {
-		res.redirect('/');
-	}).catch(err => {
-		res.status(400).send("Unable to save data");
-	});
-});
-
-app.get('/add/post', (req, res) => {
-	 res.sendFile("/addpost.html",{root: __dirname+"/views"});
-});
-
-
 app.listen(port, function () {
 	console.log("ligado");
 });
+
+app.use(express.static(__dirname + '/'));
